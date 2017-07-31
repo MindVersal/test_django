6847818a61fdf9ac8531e1209a32a3e0e42584ed
MindVersal <antajchik@gmail.com>
2017-07-31 16:17:30 +0300

    16:16 31.07.2017

--- test_django/settings.py
+++ test_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'first_test_django.apps.FirstTestDjangoConfig',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
